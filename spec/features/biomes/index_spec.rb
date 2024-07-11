25c665ba7e31d340aa030233157a3065b9ad6266
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-07-10 19:00:03 -0600

    Initial commit

---
+++ spec/features/biomes/index_spec.rb
@@ -0,0 +1,16 @@
+# User Story 1, Parent Index 
+
+# For each parent table
+# As a visitor
+# When I visit '/parents'
+# Then I see the name of each parent record in the system
+
+require "rails_helper"
+
+RSpec.describe "User Story #1 - View Parent Index", type: :feature do
+  describe "Upon visiting '/biomes'" do
+    it "shows the biome name" do
+      
+    end
+  end
+end
